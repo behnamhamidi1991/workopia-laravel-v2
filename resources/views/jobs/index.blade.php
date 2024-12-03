f142516111b27a524bac2c74d9fb7a38f458c866
behnamhamidi1991 <eslp91@hotmail.com>
2024-12-03 12:57:38 +0330

    details

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,10 +1,7 @@
 <x-layout>
-   
-    <h1 class="font-bold text-xl text-blue-700"> List of Jobs </h1>
-
         <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         @forelse($jobs as $job)
-        <div>{{$job->title}}</div>
+        <x-job-card :job="$job" />
         @empty
         <li>No jobs available!</li>
         @endforelse
